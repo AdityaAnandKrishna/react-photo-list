442cdcda25db64ff218404f97f2559a18d68a7a2
Aditya Anand <adityaananad@gmail.com>
2019-02-11 19:26:55 +0530

    UI created

--- src/MovieList.js
+++ src/MovieList.js
@@ -1,11 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
 
 class MovieList extends Component {
+  state = {
+    photos:[]
+  }
+
+  componentDidMount(){
+    axios.get("http://jsonplaceholder.typicode.com/photos")
+    .then((response) => {
+      // console.log(response.data);
+      this.setState({
+        photos: response.data.slice(0-10)
+      })
+    })
+  }
   render() {
     return (
-      <div>
-        Hello world
-      </div>
+      <section>
+        <div className="row"> 
+        {this.state.photos.map((photo) => {
+          return ( 
+              <div key={photo.id} className="col s6 m3">
+                <div className="card">
+                  <div className="card-image">
+                    <img src={photo.thumbnailUrl} alt={photo.title} />
+                    <span className="card-title">{photo.title}</span>
+                  </div>
+                  <div className="card-action">
+                    <a href="#">This is a link</a>
+                  </div>
+                </div>
+              </div>                      )
+        })}
+        </div>
+      </section>
     )
   }
 }
